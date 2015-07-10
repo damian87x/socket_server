c14c57630701998b2b004598253237c37f2245d3
Damian Borek <demianborek@gmail.com>
2015-07-10 14:32:24 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.client = function(req, res){
+  res.render('client', { id: req.params.id , title: 'Client Chat', host: req.get('host') })
+};
--- server.js
+++ server.js
@@ -1,58 +1,55 @@
 'use strict';
 
-var http       = require('http'),
-    express    = require('express'),
-    bodyParser = require ('body-parser'),
-    morgan     = require ('morgan'),
-    faye       = require('faye');
-var bayeux = new faye.NodeAdapter({
-  mount:    '/faye',
-  timeout:  45
-});
+var http = require('http'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    faye = require('faye'),
+    routes = require('./routes');
+
+
 
 var app = express();
 var server = http.createServer(app);
 
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
 
-bayeux.attach(server);
+// Configuration
 
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 app.use(morgan());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/about',function(req,res){
-    res.writeHead(302, {'Location': 'http://localhost:4200/'});
-    res.end();
+
+
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/client/:id', routes.client);
+
+
+//app.get('/client/:id', routes.client);
+
+
+// Set up chat
+
+var bayeux = new faye.NodeAdapter({
+    mount: '/faye',
+    timeout: 45
+});
+
+bayeux.attach(server);
+
+app.post('/message', function (req, res) {
+    bayeux.getClient().publish('/channel', {text: req.body.message});
+    res.send(200);
 });
 
 
-
-app.get('/client1',function(req,res){
-    res.sendFile(__dirname + '/public/client1.html');
-});
-
-app.get('/client2',function(req,res){
-    res.sendFile(__dirname + '/public/client2.html');
-});
-
-
-app.get('/', function(req, res) {
-    res.render('index', { title: 'The index page!' })
-});
-
-//app.get('/index',function(req,res){
-//    res.sendFile(__dirname + '/public/index.ejs', { title: 'ejs' });
-//});
-
-
-app.post('/message', function(req, res) {
-  bayeux.getClient().publish('/channel', {text: req.body.message});
-  res.send(200);
-});
-
-
+// Server
 
 var port = process.env.port || 8800;
 server.listen(port);
